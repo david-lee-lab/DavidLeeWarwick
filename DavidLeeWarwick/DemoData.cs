f49294a5d0ceb14fffbfff9e594ad3ce56798b0b
david-lee-lab <81710042+david-lee-lab@users.noreply.github.com>
2023-10-11 16:46:00 +0100

    display old demo data if server down

---
+++ DavidLeeWarwick/DemoData.cs
@@ -0,0 +1,338 @@
+﻿namespace DavidLeeWarwick;
+
+public class DemoData
+{
+    public const string samplejson= """
+        {
+          "sessionId": "393393d4-96b1-43ab-aebc-83870d87a4b7",
+          "series": "2023 The Quaife Mini Challenge",
+          "name": "Qualifying - Round 1",
+          "track": "Donington Park National",
+          "state": "Running",
+          "startTime": "2023-10-11T10:54:43.3851044+00:00",
+          "duration": "00:10:00",
+          "timeRemaining": "00:00:22",
+          "classification": [
+            {
+              "id": "642bee56-9c4b-4bc9-bc21-f15c99286a3c",
+              "startNumber": "21",
+              "name": "Jack Davidson",
+              "teamName": "LUX Motorsport",
+              "className": "",
+              "position": 1,
+              "finished": false,
+              "laps": 7,
+              "fastestLapTime": {
+                "display": "1:01.809",
+                "rawMs": 61809900
+              },
+              "lastLapTime": {
+                "display": "1:17.131",
+                "rawMs": 77131000
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "21.677",
+                  "rawMs": 21677200
+                },
+                "2": {
+                  "display": "31.890",
+                  "rawMs": 31890500
+                },
+                "3": {
+                  "display": "23.563",
+                  "rawMs": 23563300
+                }
+              }
+            },
+            {
+              "id": "993c3c03-07a1-4c08-aa3f-49089d247f94",
+              "startNumber": "39",
+              "name": "Lewis Brown",
+              "teamName": "Graves Motorsport",
+              "className": "",
+              "position": 2,
+              "finished": false,
+              "laps": 6,
+              "fastestLapTime": {
+                "display": "1:04.006",
+                "rawMs": 64006100
+              },
+              "lastLapTime": {
+                "display": "1:32.600",
+                "rawMs": 92600200
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "25.856",
+                  "rawMs": 25856500
+                },
+                "2": {
+                  "display": "26.817",
+                  "rawMs": 26817900
+                },
+                "3": {
+                  "display": "39.925",
+                  "rawMs": 39925800
+                }
+              }
+            },
+            {
+              "id": "70096f4b-aaf2-44dc-be19-9c17e2a10152",
+              "startNumber": "33",
+              "name": "Alex Denning",
+              "teamName": "Graves Motorsport",
+              "className": "R",
+              "position": 3,
+              "finished": false,
+              "laps": 6,
+              "fastestLapTime": {
+                "display": "1:06.162",
+                "rawMs": 66162400
+              },
+              "lastLapTime": {
+                "display": "1:06.162",
+                "rawMs": 66162400
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "17.363",
+                  "rawMs": 17363400
+                },
+                "2": null,
+                "3": null
+              }
+            },
+            {
+              "id": "29b93749-5b5b-47c6-9f4f-8336ec57fb9c",
+              "startNumber": "19",
+              "name": "Jack Mitchell",
+              "teamName": "J W Bird Motorsport",
+              "className": "",
+              "position": 4,
+              "finished": false,
+              "laps": 6,
+              "fastestLapTime": {
+                "display": "1:07.821",
+                "rawMs": 67821300
+              },
+              "lastLapTime": {
+                "display": "1:26.244",
+                "rawMs": 86244500
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "38.549",
+                  "rawMs": 38549200
+                },
+                "2": {
+                  "display": "16.131",
+                  "rawMs": 16131000
+                },
+                "3": null
+              }
+            },
+            {
+              "id": "20eab1e0-9f3e-4eda-ae5a-f9307a243fbc",
+              "startNumber": "91",
+              "name": "Robbie Dalgleish",
+              "teamName": "Robbie Dalgleish Racing (RDR)",
+              "className": "",
+              "position": 5,
+              "finished": false,
+              "laps": 6,
+              "fastestLapTime": {
+                "display": "1:08.509",
+                "rawMs": 68509700
+              },
+              "lastLapTime": {
+                "display": "1:43.926",
+                "rawMs": 103926500
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "24.731",
+                  "rawMs": 24731800
+                },
+                "2": {
+                  "display": "35.186",
+                  "rawMs": 35186600
+                },
+                "3": {
+                  "display": "44.008",
+                  "rawMs": 44008100
+                }
+              }
+            },
+            {
+              "id": "b75d1ea3-699c-4628-906b-9a34c2523a33",
+              "startNumber": "52",
+              "name": "Ru Clark",
+              "teamName": "J W Bird Motorsport",
+              "className": "G",
+              "position": 6,
+              "finished": false,
+              "laps": 5,
+              "fastestLapTime": {
+                "display": "1:09.897",
+                "rawMs": 69897100
+              },
+              "lastLapTime": {
+                "display": "1:09.897",
+                "rawMs": 69897100
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "29.485",
+                  "rawMs": 29485500
+                },
+                "2": null,
+                "3": null
+              }
+            },
+            {
+              "id": "f87a5777-0e74-47e8-a0ac-4a66350775ed",
+              "startNumber": "77",
+              "name": "Jamie Osborne",
+              "teamName": "J W Bird Motorsport",
+              "className": "R",
+              "position": 7,
+              "finished": false,
+              "laps": 5,
+              "fastestLapTime": {
+                "display": "1:13.464",
+                "rawMs": 73464600
+              },
+              "lastLapTime": {
+                "display": "1:57.164",
+                "rawMs": 117164000
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "40.012",
+                  "rawMs": 40012100
+                },
+                "2": {
+                  "display": "17.542",
+                  "rawMs": 17542600
+                },
+                "3": null
+              }
+            },
+            {
+              "id": "9971f83b-0422-4260-ac0b-04b4cedc9d55",
+              "startNumber": "71",
+              "name": "Max Coates",
+              "teamName": "Graves Motorsport",
+              "className": "",
+              "position": 8,
+              "finished": false,
+              "laps": 6,
+              "fastestLapTime": {
+                "display": "1:17.893",
+                "rawMs": 77893800
+              },
+              "lastLapTime": {
+                "display": "1:36.295",
+                "rawMs": 96295600
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "26.176",
+                  "rawMs": 26176600
+                },
+                "2": {
+                  "display": "30.428",
+                  "rawMs": 30428400
+                },
+                "3": {
+                  "display": "39.690",
+                  "rawMs": 39690600
+                }
+              }
+            },
+            {
+              "id": "80d21c1d-51bb-41d9-9f08-8de820d24525",
+              "startNumber": "315",
+              "name": "Jason Lockwood",
+              "teamName": "EXCELR8 Motorsport",
+              "className": "",
+              "position": 9,
+              "finished": false,
+              "laps": 5,
+              "fastestLapTime": {
+                "display": "1:21.970",
+                "rawMs": 81970800
+              },
+              "lastLapTime": {
+                "display": "1:45.202",
+                "rawMs": 105202300
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "44.783",
+                  "rawMs": 44783000
+                },
+                "2": null,
+                "3": null
+              }
+            },
+            {
+              "id": "e97b734b-221d-4e8a-b9c6-3d7e37d7f11c",
+              "startNumber": "987",
+              "name": "Sam Jones",
+              "teamName": "Tamworth Speed League (TSL)",
+              "className": "A",
+              "position": 10,
+              "finished": false,
+              "laps": 6,
+              "fastestLapTime": {
+                "display": "1:22.984",
+                "rawMs": 82984800
+              },
+              "lastLapTime": {
+                "display": "1:28.627",
+                "rawMs": 88627800
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "22.028",
+                  "rawMs": 22028400
+                },
+                "2": null,
+                "3": null
+              }
+            },
+            {
+              "id": "5bc722f2-d6b5-4537-b9f8-bde13281b46f",
+              "startNumber": "11",
+              "name": "Sam Weller",
+              "teamName": "Hybrid Tune",
+              "className": "G",
+              "position": 11,
+              "finished": false,
+              "laps": 5,
+              "fastestLapTime": {
+                "display": "1:27.063",
+                "rawMs": 87063600
+              },
+              "lastLapTime": {
+                "display": "1:27.063",
+                "rawMs": 87063600
+              },
+              "currentLapSectorTimes": {
+                "1": {
+                  "display": "35.894",
+                  "rawMs": 35894900
+                },
+                "2": {
+                  "display": "16.177",
+                  "rawMs": 16177100
+                },
+                "3": null
+              }
+            }
+          ]
+        }
+        """;
+}
--- DavidLeeWarwick/Pages/Index.razor.cs
+++ DavidLeeWarwick/Pages/Index.razor.cs
@@ -58,7 +58,10 @@ public partial class Index
                     }
                     using (Stream streamResp = response.Content.ReadAsStream())
                     {
-                        await UnpackData(streamResp);
+                        using (JsonDocument document = await JsonDocument.ParseAsync(streamResp))
+                        {
+                            UnpackData(document);
+                        }
                     }
                 }
             }
@@ -75,61 +78,68 @@ public partial class Index
         string[] sa = element.GetProperty(propertyName).GetString().Split(':');
         return new TimeSpan(int.Parse(sa[0]), int.Parse(sa[1]), int.Parse(sa[2]));
     }
-    async Task UnpackData(Stream streamResp)
+    void UnpackData(JsonDocument document)
     {
         drivers.Clear();
         JsonElement element;
-        using (JsonDocument document = await JsonDocument.ParseAsync(streamResp))
+        JsonElement root = document.RootElement;
+        raceseries = root.GetProperty("series").GetString();
+        drivers.Clear();
+        racename = root.GetProperty("name").GetString();
+        racetrack = root.GetProperty("track").GetString();
+        racestate = root.GetProperty("state").GetString();
+        starttime = root.GetProperty("startTime").GetDateTime();
+        duration = GetDuration(root, "duration");
+        timeremaining = GetDuration(root, "timeRemaining");
+        foreach (JsonElement classificationElement in root.GetProperty("classification").EnumerateArray())
         {
-            JsonElement root = document.RootElement;
-            raceseries = root.GetProperty("series").GetString();
-            drivers.Clear();
-            racename = root.GetProperty("name").GetString();
-            racetrack = root.GetProperty("track").GetString();
-            racestate = root.GetProperty("state").GetString();
-            starttime = root.GetProperty("startTime").GetDateTime();
-            duration = GetDuration(root, "duration");
-            timeremaining = GetDuration(root, "timeRemaining");
-            foreach (JsonElement classificationElement in root.GetProperty("classification").EnumerateArray())
+            Driver driver = new Driver();
+            driver.Name = classificationElement.GetProperty("name").GetString();
+            driver.TeamName = classificationElement.GetProperty("teamName").GetString();
+            if (classificationElement.TryGetProperty("fastestLapTime", out element))
             {
-                Driver driver = new Driver();
-                driver.Name = classificationElement.GetProperty("name").GetString();
-                driver.TeamName = classificationElement.GetProperty("teamName").GetString();
-                if (classificationElement.TryGetProperty("fastestLapTime", out element))
+                if (element.ValueKind != JsonValueKind.Null && element.TryGetProperty("display", out element))
+                    driver.FastestLap = element.GetString();
+            }
+            if (classificationElement.TryGetProperty("lastLapTime", out element))
+            {
+                if (element.ValueKind != JsonValueKind.Null && element.TryGetProperty("display", out element))
+                    driver.LastLap = element.GetString();
+            }
+            driver.Position = classificationElement.GetProperty("position").GetInt32();
+            driver.Laps = classificationElement.GetProperty("laps").GetInt32();
+            if (classificationElement.TryGetProperty("currentLapSectorTimes", out element))
+            {
+                foreach (JsonProperty innerProperty in element.EnumerateObject())
                 {
-                    if (element.ValueKind != JsonValueKind.Null && element.TryGetProperty("display", out element))
-                        driver.FastestLap = element.GetString();
-                }
-                if (classificationElement.TryGetProperty("lastLapTime", out element))
-                {
-                    if (element.ValueKind != JsonValueKind.Null && element.TryGetProperty("display", out element))
-                        driver.LastLap = element.GetString();
-                }
-                driver.Position = classificationElement.GetProperty("position").GetInt32();
-                driver.Laps = classificationElement.GetProperty("laps").GetInt32();
-                if (classificationElement.TryGetProperty("currentLapSectorTimes", out element))
-                {
-                    foreach (JsonProperty innerProperty in element.EnumerateObject())
+                    int sector;
+                    if (int.TryParse(innerProperty.Name, out sector))
                     {
-                        int sector;
-                        if (int.TryParse(innerProperty.Name, out sector))
+                        if (sector > 0 && sector <= 3)
                         {
-                            if (sector > 0 && sector <= 3)
-                            {
-                                if (innerProperty.Value.ValueKind != JsonValueKind.Null && innerProperty.Value.TryGetProperty("display", out element))
-                                    driver.Sectors[sector - 1] = element.GetString();
-                            }
+                            if (innerProperty.Value.ValueKind != JsonValueKind.Null && innerProperty.Value.TryGetProperty("display", out element))
+                                driver.Sectors[sector - 1] = element.GetString();
                         }
                     }
                 }
-                drivers.Add(driver);
             }
+            drivers.Add(driver);
         }
     }
     async protected override Task OnInitializedAsync()
     {
         drivers = new(50);
         await ReadData();
+
+        //Show static demo data from earlier
+        if (polled == DateTime.MinValue)
+        {
+            using (JsonDocument jsonDocument= JsonDocument.Parse(DemoData.samplejson))
+            {
+                UnpackData(jsonDocument);
+            }
+            polled = DateTime.UtcNow;
+        }
 
         //Timer
         timer = new System.Timers.Timer();
